ab4f07546597e930e0fef366a47511cda865982e
Zain <zmathews@student.wethinkcode.co.za>
2020-08-23 13:21:09 +0200

    how is this supposed to function?

---
+++ weather/Coordinates.java
@@ -0,0 +1,19 @@
+package weather;
+
+public class Coordinates {
+	int longitude;
+	int latitude;
+	int height;
+
+	public static int getLongitude(int longitude) {
+		return longitude;
+	}
+
+	public static int getLatitude(int latitude) {
+		return latitude;
+	}
+
+	public static int getHeight(int height) {
+		return height;
+	}
+}
