54b62340c778606ee4384b9e6235286b5ac86efc
Zain <zmathews@student.wethinkcode.co.za>
2020-08-27 18:33:33 +0200

    2 seconds of thinking

--- simulator/Simulator.java
+++ simulator/Simulator.java
@@ -13,10 +13,10 @@ public class Simulator {
 	// Coordinates coordinates = new Coordinates(20, 30, 10);
 	// Should be where everything gets put together??
 	public static void main(String[] args) throws StandardException, IOException, FileNotFoundException {
-		// read file
-		// error handle file
-		// split lines
-			// first line == number of simulations
+		// read file										done
+		// error handle file								wip
+		// split lines										done
+			// first line == number of simulations			wip
 			// everyline after 5 instances
 			// first is aircraft name
 			// second is aircraft number
@@ -29,15 +29,17 @@ public class Simulator {
 			if (args.length != 1) {
 				throw new StandardException("Incorrect number of parameters. Use a single parameter of type file path");
 			}
+			else {
 				BufferedReader br = new BufferedReader(new FileReader(args[0]));
 				String line = "";
 				int lineNum = 0;
+				String arr[] = null;
+				int totalSims = 0;
 				while (line != null) {
-					// String arr[] = line.split(" ");
+					arr = line.split(" ");
 					if (lineNum == 1) {
-						final int totalSims = Integer.parseInt(line);
+						totalSims = Integer.parseInt(line);
 						// System.out.println("Am here:" + line + "heh");
-						System.out.println(totalSims);
 					}
 					line = br.readLine();
 					if (line == null || line.length() == 0) {
@@ -46,8 +48,15 @@ public class Simulator {
 					// System.out.println(line.length());
 					// System.out.println(line);
 					lineNum++;
+					// System.out.println(arr);
 				}
+				System.out.println(totalSims);
+				System.out.println(arr.length);
+				// for (String aircraft : arr) {
+				// 	System.out.println(aircraft);
+				// }
 				br.close();
+			}
 			} catch (FileNotFoundException e) {
 				System.out.println("File not found");
 			} catch (StandardException e) {
