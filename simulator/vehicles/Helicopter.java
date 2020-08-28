86da2db3a2ada596bf229eb0d44cf8678dc69dd0
Zain <zmathews@student.wethinkcode.co.za>
2020-08-28 22:18:30 +0200

    feels weather

--- simulator/vehicles/Helicopter.java
+++ simulator/vehicles/Helicopter.java
@@ -11,7 +11,22 @@ public class Helicopter extends Aircraft implements Flyable {
 	}
 
 	public void updateConditions() {
-
+		String weather = weatherTower.getWeather(coordinates);
+		if (weather.equals("RAIN")) {
+			this.coordinates = new Coordinates(this.coordinates.getLongitude() + 5, this.coordinates.getLatitude(), this.coordinates.getHeight());
+		}
+		else if (weather.equals("FOG")) {
+			this.coordinates = new Coordinates(this.coordinates.getLongitude() + 1, this.coordinates.getLatitude(), this.coordinates.getHeight());
+		}
+		else if (weather.equals("SUN")) {
+			this.coordinates = new Coordinates(this.coordinates.getLongitude() + 10, this.coordinates.getLatitude(), this.coordinates.getHeight() + 2);
+		}
+		else if (weather.equals("SNOW")) {
+			this.coordinates = new Coordinates(this.coordinates.getLongitude(), this.coordinates.getLatitude(), this.coordinates.getHeight() - 12);
+		}
+		if (this.coordinates.getHeight() == 0) {
+			this.weatherTower.unregister(this);
+		}
 	}
 
 	public void registerTower(WeatherTower weatherTower) {
--- simulator/vehicles/JetPlane.java
+++ simulator/vehicles/JetPlane.java
@@ -11,15 +11,26 @@ public class JetPlane extends Aircraft implements Flyable {
 	}
 
 	public void updateConditions() {
-
+		String weather = weatherTower.getWeather(coordinates);
+		if (weather.equals("RAIN")) {
+			this.coordinates = new Coordinates(this.coordinates.getLongitude(), this.coordinates.getLatitude() + 5, this.coordinates.getHeight());
+		}
+		else if (weather.equals("FOG")) {
+			this.coordinates = new Coordinates(this.coordinates.getLongitude(), this.coordinates.getLatitude() + 1, this.coordinates.getHeight());
+		}
+		else if (weather.equals("SUN")) {
+			this.coordinates = new Coordinates(this.coordinates.getLongitude(), this.coordinates.getLatitude() + 10, this.coordinates.getHeight() + 2);
+		}
+		else if (weather.equals("SNOW")) {
+			this.coordinates = new Coordinates(this.coordinates.getLongitude(), this.coordinates.getLatitude(), this.coordinates.getHeight() - 7);
+		}
+		if (this.coordinates.getHeight() == 0) {
+			this.weatherTower.unregister(this);
+		}
 	}
 
 	public void registerTower(WeatherTower weatherTower) {
 		this.weatherTower = weatherTower;
 		this.weatherTower.register(this);
 	}
-
-	// public void test() {
-
-	// }
 }
