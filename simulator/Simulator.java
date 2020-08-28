54e9810ecdfed7145db2e91c06d670f37763d8b8
Zain MATHEWS <zmathews@c4r14s4.wethinkcode.co.za>
2020-08-28 12:11:20 +0200

    can't test fuck knows if it does anything

--- simulator/Simulator.java
+++ simulator/Simulator.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.File;
+import java.io.FileWriter;
 
 // import weather.Coordinates;
 import simulator.exceptions.StandardException;
@@ -36,23 +38,54 @@ public class Simulator {
 				int lineNum = 0;
 				String arr[] = null;
 				int totalSims = 0;
+				String acType = "";
+				String acName = "";
+				int lat = 0;
+				int longi = 0;
+				int height = 0;
+				// get totalSims value
 				while (line != null) {
 					arr = line.split(" ");
 					if (lineNum == 1) {
 						totalSims = Integer.parseInt(line);
 						// System.out.println("Am here:" + line + "heh");
+						lineNum++;
+						break ;
 					}
 					line = br.readLine();
 					if (line == null || line.length() == 0) {
-						continue;
+						continue ;
 					}
 					// System.out.println(line.length());
 					// System.out.println(line);
 					lineNum++;
 					// System.out.println(arr);
 				}
+				// handle simulations
+				while (line != null) {
+					arr = line.split(" ");
+					if (arr.length != 5 || arr.length != 0) {
+						continue ;
+					}
+					if (arr.length == 5) {
+						acType = arr[0];
+						acName = arr[1];
+						lat = Integer.parseInt(arr[2]);
+						longi = Integer.parseInt(arr[3]);
+						height = Integer.parseInt(arr[4]);
+						File newFile = new File("simulations.txt");
+						FileWriter writeTo = new FileWriter(newFile);
+						writeTo.write(acType);
+						writeTo.write(acName);
+						writeTo.write(lat);
+						writeTo.write(longi);
+						writeTo.write(height);
+						writeTo.close();
+					}
+					System.out.println(arr);
+				}
 				System.out.println(totalSims);
-				System.out.println(arr.length);
+				// System.out.println(arr.length);
 				// for (String aircraft : arr) {
 				// 	System.out.println(aircraft);
 				// }
@@ -60,6 +93,8 @@ public class Simulator {
 			}
 			} catch (FileNotFoundException e) {
 				System.out.println("File not found");
+			} catch (IOException e){
+				System.out.println(e.getMessage());
 			} catch (StandardException e) {
 				System.out.println(e.getMessage());
 			}
